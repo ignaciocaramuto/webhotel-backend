f15d506b762f0124a80c04960fa3bbeb54487d3b
Nacho <ignaciocaramuto@gmail.com>
2023-02-09 17:03:33 -0300

    update: metodos create put delete de cliente

--- src/main/java/com/tp/webhotel/controller/ClienteController.java
+++ src/main/java/com/tp/webhotel/controller/ClienteController.java
@@ -1,12 +1,11 @@
 package com.tp.webhotel.controller;
 
+import java.sql.Date;
 import java.util.List;
 import com.tp.webhotel.model.Cliente;
 import com.tp.webhotel.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/cliente")
@@ -21,6 +20,30 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> getClientes() {
         return clienteService.getClientes();
+    }
+
+    @PostMapping
+    public void registrarNuevoCliente(@RequestBody Cliente cliente){
+        clienteService.agregarNuevoCliente(cliente);
+    }
+
+    @PutMapping(path = "{clienteId}")
+    public void actualizarCliente(
+            @PathVariable("clienteId") int id,
+            @RequestParam(required = false) String apellido,
+            @RequestParam(required = false) String nombre,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String clave,
+            @RequestParam(required = false) Date fechaNacimiento,
+            @RequestParam(required = false) int nroTarjetaCredito) {
+                clienteService.actualizarCliente(
+                        id, apellido, nombre, email, clave, fechaNacimiento, nroTarjetaCredito
+                );
+    }
+
+    @DeleteMapping(path = "{clienteId}")
+    public void eliminarCliente(@PathVariable("clienteId") int id){
+        clienteService.eliminarCliente(id);
     }
 
 }
--- src/main/java/com/tp/webhotel/service/ClienteService.java
+++ src/main/java/com/tp/webhotel/service/ClienteService.java
@@ -1,10 +1,16 @@
 package com.tp.webhotel.service;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 import com.tp.webhotel.model.Cliente;
 import com.tp.webhotel.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class ClienteService {
@@ -20,4 +26,60 @@ public class ClienteService {
         return clienteRepository.findAll();
     }
 
+    public void agregarNuevoCliente(Cliente cliente) {
+        // Optional porque puede no existir
+        Optional<Cliente> clientePorEmail = clienteRepository.findClienteByEmail(cliente.getEmail());
+        if (clientePorEmail.isPresent()){
+            throw new IllegalStateException("El email ya fue registrado.");
+        }
+        clienteRepository.save(cliente);
+    }
+
+    public void eliminarCliente(int id) {
+        boolean existe = clienteRepository.existsById(id);
+        if (!existe) {
+            throw new IllegalStateException("El cliente con id " + id + " no existe.");
+        }
+        clienteRepository.deleteById(id);
+    }
+
+    @Transactional /* Permite no utilizar querys y usar los setters de la entidad y
+    usarlos para actualizarlos automaticamente cuando es posible */
+    public void actualizarCliente(
+            int id,
+            String apellido,
+            String nombre,
+            String email,
+            String clave,
+            Date fechaNacimiento,
+            int nroTarjetaCredito) {
+       Cliente cliente = clienteRepository.findById(id)
+               .orElseThrow(() -> new IllegalStateException(
+                       "El cliente con id " + id + " no existe."
+       ));
+
+       if (apellido != null && apellido.length() > 0 && !Objects.equals(cliente.getApellido(), apellido)) {
+           cliente.setApellido(apellido);
+       }
+       if (nombre != null && nombre.length() > 0 && !Objects.equals(cliente.getNombre(), nombre)) {
+           cliente.setNombre(nombre);
+       }
+       // TODO: Agregar validacián de email válido
+       if (email != null && email.length() > 0 && !Objects.equals(cliente.getEmail(), email)) {
+           Optional<Cliente> clientePorEmail = clienteRepository.findClienteByEmail(email);
+           if (clientePorEmail.isPresent()){
+               throw new IllegalStateException("El email ya está tomado.");
+           }
+           cliente.setEmail(email);
+       }
+       if (clave != null && clave.length() > 0 && !Objects.equals(cliente.getClave(), clave)) {
+           cliente.setClave(clave);
+       }
+       if (fechaNacimiento != null && !Objects.equals(cliente.getFechaNacimiento(), fechaNacimiento)) {
+           cliente.setFechaNacimiento(fechaNacimiento);
+       }
+       if (!Objects.equals(cliente.getTarjetaCredito(), nroTarjetaCredito)) {
+           cliente.setTarjetaCredito(nroTarjetaCredito);
+       }
+    }
 }
